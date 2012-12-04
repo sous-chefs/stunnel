df01b2b92b62e61405f25f4f84f3f5fcbc5b0ff9
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-04 15:32:33 -0800

    Add more configuration options. Provide explicit server recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,9 @@
-package "stunnel"
+node[:stunnel][:packages].each do |s_pkg|
+  package s_pkg
+end
 
 # Create directory to hold the pid inside the chroot jail
-if node[:stunnel][:use_chroot]
+if(node[:stunnel][:use_chroot])
   directory "#{node[:stunnel][:chroot_path]}" do
     owner node[:stunnel][:user]
     group node[:stunnel][:group]
@@ -12,42 +14,16 @@ end
 
 template "/etc/stunnel/stunnel.conf" do
   source "stunnel.conf.erb"
-  owner "root"
-  group "root"
-  mode "0644"
+  mode 0644
 end
 
-service "stunnel4" do
+template "/etc/default/stunnel4" do
+  source "stunnel.default.erb"
+  mode 0644
+end
+
+service "stunnel" do
+  service_name node[:stunnel][:service_name]
   supports :status => true, :restart => true, :reload => true
   action [ :enable, :start ]
 end
-
-bash "Create SSL Certificates" do
-	# Steps
-	# =====
-	#	1) Create an SSL private key.
-	#	2) Create certificate signing request (CSR).
-	#
-	# @param node[:fqdn]: The hostname or fqdn of the server.
-	# @param node[:stunnel][:server_ssl_req]
-
-	cwd "/etc/ssl"
-    creates "/etc/ssl/certs/stunnel.pem"
-	code <<-EOH
-	umask 077
-	openssl genrsa 2048 > private/#{node[:fqdn]}.key
-	openssl req -subj "#{node[:stunnel][:server_ssl_req]}" -new -x509 -nodes -sha1 -days 3650 -key private/#{node[:fqdn]}.key > certs/#{node[:fqdn]}.crt
-	cat private/#{node[:fqdn]}.key certs/#{node[:fqdn]}.crt > certs/stunnel.pem
-	EOH
-end
-
-
-file "/etc/default/stunnel4" do
-  content <<-EOS
-    ENABLED=1
-    FILES='/etc/stunnel/*.conf'
-    OPTIONS=''
-  EOS
-end
-
-
