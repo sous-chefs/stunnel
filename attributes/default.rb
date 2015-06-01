8ff96a7329af1973f29d3201e3f7f4e255e27506
Nick Lopez <nlopez@amplify.com>
2015-05-31 22:09:56 -0400

    add boolen attribute to control whether stunnel forks or not

--- attributes/default.rb
+++ attributes/default.rb
@@ -43,6 +43,7 @@ default['stunnel']['pidfile'] = '/tmp/stunnel.pid'
 default['stunnel']['user'] = 'root'
 default['stunnel']['group'] = 'root'
 default['stunnel']['ulimit'] = nil
+default['stunnel']['foreground'] = false
 
 default['stunnel']['https']['enabled'] = false
 default['stunnel']['https']['accept_port'] = '443'
