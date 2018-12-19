0ffb5fdc1da007614b2c4bb6c2505eb0207965f4
Dan Webb <dan.webb@damacus.io>
2018-12-19 12:56:07 +0000

    Release 4.0.0 (#47)

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,16 @@
-name             'stunnel' # ~FC121
-maintainer       'DNSimple Corp'
-maintainer_email 'ops@dnsimple.com'
+name             'stunnel'
+maintainer       'Sous-Chefs'
+maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
 description      'Provides resources to help install and configure stunnel'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url       'https://github.com/aetrion/chef-stunnel'
-issues_url       'https://github.com/aetrion/chef-stunnel/issues'
-version          '3.1.1'
+source_url       'https://github.com/sous-chefs/chef-stunnel'
+issues_url       'https://github.com/sous-chefs/chef-stunnel/issues'
+version          '4.0.0'
+chef_version     '>= 13'
 
-chef_version '>= 12.6' if respond_to?(:chef_version)
 supports 'ubuntu'
 supports 'centos'
+supports 'fedora'
+supports 'suse'
 
 depends 'build-essential'
