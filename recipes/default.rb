7a0f8bc76905773bf39c45ad85405c2282dd48ef
Dan Webb <dan.webb@damacus.io>
2018-12-19 11:15:04 +0000

    Remove default option which breaks stunnel config in newer stunnel versions

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,11 +19,6 @@
 
 apt_update 'update'
 
-log 'deprecation warning' do
-  message 'This is the last release to support Chef 12. 4.0.0 will be Chef 13+ only so please version pin if you need to. Please see README with upgrade instructions in the new release.'
-  level :warn
-end
-
 if node['stunnel']['install_method'] == 'source'
   include_recipe 'stunnel::source'
 else
